cec18a15e0d7bd7b863b049db6692e169d5cf77f
wyh <yale_wan@163.com>
2020-06-24 17:53:17 +0800

    feat: 初始化项目

---
+++ config/config.ts
@@ -0,0 +1,84 @@
+import { defineConfig } from 'umi';
+import defaultSettings from './defaultSettings';
+import proxy from './proxy';
+
+const { REACT_APP_ENV } = process.env;
+
+export default defineConfig({
+  hash: true,
+  antd: {},
+  dva: {
+    hmr: true,
+  },
+  locale: {
+    default: 'zh-CN',
+    antd: true,
+    baseNavigator: true,
+  },
+  dynamicImport: {
+    loading: '@/components/PageLoading/index',
+  },
+  targets: {
+    ie: 11,
+  },
+  routes: [
+    {
+      path: '/user',
+      component: '../layouts/UserLayout',
+      routes: [
+        {
+          name: 'login',
+          path: '/user/login',
+          component: './user/login',
+        },
+      ],
+    },
+    {
+      path: '/',
+      component: '../layouts/SecurityLayout',
+      routes: [
+        {
+          path: '/',
+          component: '../layouts/BasicLayout',
+          authority: ['admin', 'user'],
+          routes: [
+            {
+              path: '/',
+              redirect: '/dashbord',
+            },
+            {
+              path: '/dashbord',
+              name: 'dashbord',
+              icon: 'smile',
+              component: './Dashbord',
+            },
+            {
+              name: 'list.table-list',
+              icon: 'table',
+              path: '/list',
+              component: './ListTableList',
+            },
+            {
+              component: './404',
+            },
+          ],
+        },
+        {
+          component: './404',
+        },
+      ],
+    },
+    {
+      component: './404',
+    },
+  ],
+  theme: {
+    'primary-color': defaultSettings.primaryColor,
+  },
+  title: false,
+  ignoreMomentLocale: true,
+  proxy: proxy[REACT_APP_ENV || 'dev'],
+  manifest: {
+    basePath: '/',
+  },
+});
---
+++ src/pages/Dashbord.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+export default (): React.ReactNode => (
+  <PageHeaderWrapper>
+    首页1111111111
+  </PageHeaderWrapper>
+);
